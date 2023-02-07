301e98dd66bc59b5d779470ff41d73def57ff37c
Omer Ozceylan <omerozceylan40@gmail.com>
2023-02-08 00:36:19 +0300

    add: linkedin link

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-let text = new Typed("#text",{
-    strings:["Frontend Dev.", "Open To Work"],
-    typeSpeed:30,
-    backSpeed:15,
-    loop:true
+// let downButton = document.querySelectorAll('.card-button-down')
+
+// downButton.forEach(element => {
+//     element.addEventListener('click',function(){
+//       card = element.parentNode.parentNode.parentNode
+//       let imgs = card.querySelector('.project-card-img')
+//       card.classList.toggle('expanded-card')
+//       imgs.classList.toggle('expanded')
+      
+//     })
+// })
+
+
+
+
+
+
+/*
+let downButton = document.querySelectorAll('.card-button-down')
+
+downButton.forEach((element)=>{
+    element.addEventListener('click',changeWitdh)
 })
+
+function changeWitdh(){
+    let projectImgs = document.querySelectorAll('.project-images')
+
+}
+*/
---
+++ text.js
@@ -0,0 +1,7 @@
+let text = new Typed("#text", {
+    strings: ["Frontend Dev.", "Open To Work"],
+    typeSpeed: 30,
+    backSpeed: 15,
+    loop: true,
+  });
+  
