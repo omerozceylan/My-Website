c450aa317ca4c95a6e39f2b8e786c1c193813fc5
Omer Ozceylan <omerozceylan40@gmail.com>
2023-03-10 07:21:42 +0300

    add: data.js file

---
+++ data.js
@@ -0,0 +1,30 @@
+const projects = [
+    {
+        title:'Calculator',
+        badges:[
+            'js', 'html', 'css'
+        ],
+        info:'A simple calculator with functions for 4 arithmetic operations and percentage calculation.'
+    },
+    {
+        title:'Medium Clone',
+        badges:[
+            'js', 'html', 'css'
+        ],
+        info:'A simple calculator with functions for 4 arithmetic operations and percentage calculation.'
+    },
+    {
+        title:'Counter',
+        badges:[
+            'js', 'html', 'css'
+        ],
+        info:'A simple calculator with functions for 4 arithmetic operations and percentage calculation.'
+    },
+    {
+        title:'To Do List',
+        badges:[
+            'js', 'html', 'css'
+        ],
+        info:'A simple calculator with functions for 4 arithmetic operations and percentage calculation.'
+    }
+]
